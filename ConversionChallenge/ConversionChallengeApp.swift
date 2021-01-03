d0f667804fda55402ee822df4f92bcaa4c9cb47f
Scott Obara <scott@rubbscreations.com>
2021-01-03 17:59:14 +1100

    Initial Commit

---
+++ ConversionChallenge/ConversionChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionChallengeApp.swift
+//  ConversionChallenge
+//
+//  Created by Scott Obara on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ConversionChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
